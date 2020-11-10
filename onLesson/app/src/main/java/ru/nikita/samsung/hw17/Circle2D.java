f405d961af46da0c547726f08b624054aa98f833
Nikitarg1 <nikita.gomozov@gmail.com>
2020-11-10 20:51:04 +0300

    - добавил отскок от стенок - добавил изменение цвета, когда шарики они косаются стенок

--- onLesson/app/src/main/java/ru/nikita/samsung/hw17/Circle2D.java
+++ onLesson/app/src/main/java/ru/nikita/samsung/hw17/Circle2D.java
@@ -3,36 +3,27 @@ package ru.nikita.samsung.hw17;
 import android.graphics.Color;
 
 public class Circle2D {
-    float x;
-    float y;
-
-    float dX;
-    float dY;
-
-    float radius;
+    float x, y, radius;
     int color;
+    float dX, dY;
 
     public Circle2D() {
-        x=0;
-        y=0;
-        dX=0;
-        dY=0;
-        radius=10;
+        x = 0;
+        y = 0;
+        radius = 50;
         color = Color.BLUE;
     }
 
-    public Circle2D(float x, float y, float dx, float dy, float radius, int color){
-        this.x=x;
-        this.y=y;
-
-        this.dX=x;
-        this.dY=y;
-
-        this.radius=10;
+    public Circle2D(float radius, float x, float y, int color) {
+        this.x = x;
+        this.y = y;
+        this.radius = 10;
         this.color = Color.BLUE;
+        this.dX = x;
+        this.dY = y;
     }
 
-    public void Draw(){
-        //TODO: реализовать отрисовку круга
+    public void Draw() {
+        //TODO релизовать отрисовку круга
     }
 }
--- onLesson/app/src/main/java/ru/nikita/samsung/hw17/MyView.java
+++ onLesson/app/src/main/java/ru/nikita/samsung/hw17/MyView.java
@@ -2,64 +2,79 @@ package ru.nikita.samsung.hw17;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class MyView extends View {
+import java.util.Random;
 
+import static java.lang.Math.random;
+
+public class MyView extends View {
+    Random rand = new Random();
     Paint paint = new Paint();
     Circle2D[] arrayCircleList;
 
-    float _remeber[] = new float[2];
-    float _delta[] = new float[2];
+    float[] remebr = new float[2];
+    float[] delta = new float[2];
 
-    public MyView(Context context, int countOfCircle){
+    public MyView(Context context, int countOfCircle) {
         super(context);
         arrayCircleList = new Circle2D[countOfCircle];
-
-        for (int i=0; i<arrayCircleList.length; i++){
+        for (int i = 0; i < arrayCircleList.length; i++) {
             arrayCircleList[i] = new Circle2D();
-            Log.d("Debug", Integer.toString(getWidth()));
-            arrayCircleList[i].x = (float)Math.random()*500;/* getWidth() */;
-            arrayCircleList[i].dX = (float)Math.random();/* getWidth() */;
-            arrayCircleList[i].dY = (float)Math.random();/* getWidth() */;
+            arrayCircleList[i].x = 540;
+            arrayCircleList[i].y = 990;
+            arrayCircleList[i].dX = (float) (random());
+            arrayCircleList[i].dY = (float) (random());
+            arrayCircleList[i].color = RandomColor(arrayCircleList[i].color);
+            arrayCircleList[i].radius = rand.nextInt(50) + 50;
         }
     }
-
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        for (int i=0; i<arrayCircleList.length; i++){
-            Circle2D _circle = arrayCircleList[i];
+        for (Circle2D _circle : arrayCircleList) {
             paint.setColor(_circle.color);
             canvas.drawCircle(_circle.x, _circle.y, _circle.radius, paint);
-            _circle.y += _circle.dY * _delta[1];
-            _circle.x += _circle.dX * _delta[0];
+            _circle.y += _circle.dY * delta[1];
+            _circle.x += _circle.dX * delta[0];
+
+            if ((_circle.x + _circle.dX) > getWidth() - _circle.radius || _circle.x + _circle.dX < _circle.radius) {
+                _circle.dX = -_circle.dX;
+                _circle.color = RandomColor(_circle.color);
+            }
+
+            if ((_circle.y + _circle.dY) > getHeight() - _circle.radius || _circle.y + _circle.dY < _circle.radius) {
+                _circle.dY = -_circle.dY;
+                _circle.color = RandomColor(_circle.color);
+            }
         }
-/*
-        x = canvas.getWidth()/2;
-        canvas.drawCircle(x, y, 20, paint);
-        //x += 0.5f;
-        y += 0.5f;
- */
         invalidate();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                _remeber[0] = event.getX();
-                _remeber[1] = event.getY();
+                remebr[0] = event.getX();
+                remebr[1] = event.getY();
                 break;
             case MotionEvent.ACTION_UP:
-                _delta[0] = event.getX()-_remeber[0];
-                _delta[1] = event.getY()-_remeber[1];
+                delta[0] = event.getX() - remebr[0];
+                delta[1] = event.getY() - remebr[1];
                 break;
         }
         return true;
     }
+
+    public int RandomColor(int color) {
+        int r = rand.nextInt(255);
+        int g = rand.nextInt(255);
+        int b = rand.nextInt(255);
+        return color = Color.rgb(r, g, b);
+    }
+
 }
